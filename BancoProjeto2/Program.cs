8e137c97e353c5767e33b36d1714e4080aa1733f
Pedro Cabral <Pedrohgc16@outlook.com>
2023-01-09 19:53:04 -0400

    Adicionar arquivos de projeto.

---
+++ BancoProjeto2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Cadastre seu nome de Usuário e senha:");
+Console.Write("Nome de Usuário: ");
+string user = Console.ReadLine();
+Console.Write("Senha: ");
+string senha = Console.ReadLine();
