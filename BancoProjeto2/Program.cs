1cdbb7a6c420aedab2a29ce52848400ad458b4bf
Pedro Cabral <Pedrohgc16@outlook.com>
2023-01-11 00:13:32 -0400

    MRP

--- BancoProjeto2/Program.cs
+++ BancoProjeto2/Program.cs
@@ -1,5 +1,12 @@
-﻿Console.WriteLine("Cadastre seu nome de Usuário e senha:");
+﻿Console.WriteLine("Cadastre seu nome de usuário e senha:");
 Console.Write("Nome de Usuário: ");
 string user = Console.ReadLine();
 Console.Write("Senha: ");
 string senha = Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine("Faça seu Login com usuário e senha: ");
+Console.Write("Nome de Usuário: ");
+string checkUser = Console.ReadLine();
+Console.Write("Senha: ");
+string checkSenha = Console.ReadLine();
