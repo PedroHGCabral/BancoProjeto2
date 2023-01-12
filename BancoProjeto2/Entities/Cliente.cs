6d7d95563ed3e12b3add38767a95fe129e30af44
Pedro Cabral <Pedrohgc16@outlook.com>
2023-01-11 20:43:38 -0400

    Adições de classe e metodos para cadastro de clientes e geração de erros

---
+++ BancoProjeto2/Entities/Cliente.cs
@@ -0,0 +1,71 @@
+﻿
+
+namespace BancoProjeto2.Entities
+{
+    internal class Cliente
+    {
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public List<Cliente> Contas { get; set; }
+
+        public Cliente()
+        {
+            Contas = new List<Cliente>();
+        }
+
+        public Cliente(string usuario, string senha)
+        {
+            Usuario = usuario;
+            Senha = senha;
+        }
+
+        public string CadastrarCliente(string usuario, string senha)
+        {
+            // verificação de segurança
+            if (usuario.Length < 4 || senha.Length < 4)
+            {
+                return "002"; /// 002 - Nome de usuário e senha precisam ter 4 caracteres ou mais
+            }
+
+            // Verificação de disponibilidade de nome de usuário
+            bool exists = Contas.Exists(x => x.Usuario == usuario);
+            if (exists == true)
+            {
+                return "001"; /// 001 - Usuário já existente
+            }
+            // Adição de usuário e senha a lista Conta
+            else
+            {
+                Usuario = usuario;
+                Senha = senha;
+                Contas.Add(new Cliente(Usuario, Senha));
+                return "000"; /// 000 - Cadastrado com sucessos
+            }
+        }
+
+        public string VerificarLogin(string usuario, string senha)
+        {
+            return "";
+        }
+
+        public string VerificarErro(string codigo)
+        {
+            if (codigo == "000")
+            {
+                return "Cadastrado com sucesso";
+            }
+            else if (codigo == "001")
+            {
+                return "Usuário já existente";
+            }
+            else if (codigo == "002")
+            {
+                return "Nome de usuário e senha precisam ter 4 caracteres ou mais";
+            }
+            else
+            {
+                return "Erro Desconhecido";
+            }
+        }
+    }
+}
--- BancoProjeto2/Program.cs
+++ BancoProjeto2/Program.cs
@@ -1,12 +1,28 @@
-﻿Console.WriteLine("Cadastre seu nome de usuário e senha:");
-Console.Write("Nome de Usuário: ");
-string user = Console.ReadLine();
-Console.Write("Senha: ");
-string senha = Console.ReadLine();
-Console.Clear();
+﻿using BancoProjeto2.Entities;
 
-Console.WriteLine("Faça seu Login com usuário e senha: ");
-Console.Write("Nome de Usuário: ");
-string checkUser = Console.ReadLine();
-Console.Write("Senha: ");
-string checkSenha = Console.ReadLine();
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string? codigo;
+        Cliente cliente = new Cliente();
+
+        Console.WriteLine("Cadastre seu nome de usuário e senha:");
+        Console.Write("Nome de Usuário: ");
+        string user = Console.ReadLine();
+        Console.Write("Senha: ");
+        string senha = Console.ReadLine();
+
+        codigo = cliente.CadastrarCliente(user, senha);
+        Console.WriteLine(cliente.VerificarErro(codigo));
+        Console.ReadKey();
+
+        Console.Clear();
+
+        Console.WriteLine("Faça seu Login com usuário e senha: ");
+        Console.Write("Nome de Usuário: ");
+        string checkUser = Console.ReadLine();
+        Console.Write("Senha: ");
+        string checkSenha = Console.ReadLine();
+    }
+}
